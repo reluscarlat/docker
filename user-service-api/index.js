02754b2e67c5a411c64c91716e51d6040126998c
Relu Scarlat <relu.scarlat@gmail.com>
2021-09-26 15:14:35 +0300

    Initial commit

---
+++ user-service-api/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      name: 'Bob',
+      email: 'bob@gmail.com' 
+    },
+    {
+        name: 'Alice',
+        email: 'alice@hotmail.com' 
+    }
+])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
